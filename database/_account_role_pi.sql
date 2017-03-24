1e9679379e75277de89cb8aa783c16f797640bbf
Damon V. Caskey <dc@caskeys.com>
2017-03-23 20:40:03 -0400

    Delete _account_role_pi.sql

--- database/_account_role_pi.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tbl_account_role
-(fk_id, item)
-SELECT id,  FROM tbl_account AS _account
-JOIN
-	tbl_master AS _master ON _master.id_key = _account.id_key
-WHERE _master.id NOT IN(933, 117, 175, 191, 256, 352, 418, 460, 650, 684, 699, 744) 
