d1529b6b8beffc96fbd02f3c6916f70b4c4dc80b
Damon V. Caskey <dc@caskeys.com>
2017-07-16 17:11:42 -0400

    Update to latest Yukon.

---
+++ gen_error.php
@@ -0,0 +1,45 @@
+<?php 
+
+	// Central config and libraries.
+	//require(__DIR__.'/source/main.php');
+?>
+
+<!DOCtype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Inspector Blair</title>        
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link rel="stylesheet" href="source/css/style.css" />
+        <link rel="stylesheet" href="source/css/print.css" media="print" />
+        
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    </head>
+    
+    <body>          
+        <div id="container" class="container">       
+                                                                                           
+            <div class="page-header">
+                <h1>Inspector Blair</h1>
+                <p class="lead alert-danger">Error - Yukon Database module cannot establish connection, host unavailable. For more information, please contact <a href="mailto:dvcask2@uky.edu">Caskey, Damon V.</a> at 859-257-3241.</p>
+			</div>
+                    
+           
+        </div><!--container-->        
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40196994-1', 'uky.edu');
+  ga('send', 'pageview');
+</script>
+</body>
+</html>
