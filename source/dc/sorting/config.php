d7b76adf1e1fcfdbdefa17aaa921a3737498098a
Damon V. Caskey <dc@caskeys.com>
2017-07-26 19:19:51 -0400

    Refine constant list for sorting and add target field identifiers.

--- source/dc/sorting/config.php
+++ source/dc/sorting/config.php
@@ -6,24 +6,27 @@
 	{
 		// If these constant values are changed, make sure to update
 		// the mutator methods as well.
-		const
-			ORDER	= 'order',
-			FILTER	= 'field';
+		const ORDER		= 'order';
+		const FILTER 	= 'field';
 	}
 	
 	abstract class ORDER_MARKUP
 	{
-		const 		
-			ASCENDING	= '<span class="glyphicon glyphicon glyphicon-sort-by-alphabet"></span>',
-			DECENDING	= '<span class="glyphicon glyphicon glyphicon-sort-by-alphabet-alt"></span>',
-			NONE 		= '<span class="glyphicon glyphicon glyphicon-sort"></span>';
+		const ASCENDING	= '<span class="glyphicon glyphicon glyphicon-sort-by-alphabet"></span>';
+		const DECENDING	= '<span class="glyphicon glyphicon glyphicon-sort-by-alphabet-alt"></span>';
+		const NONE 		= '<span class="glyphicon glyphicon glyphicon-sort"></span>';
 	}
 	
 	abstract class ORDER_TYPE
 	{
-		const 
-			ASCENDING 	= 0,
-			DECENDING	= 1;			
+		const ASCENDING = 0;
+		const DECENDING	= 1;			
+	}
+
+	abstract class FIELD
+	{
+		const REVISION	= 1;
+		const LABEL 	= 2;
 	}
 
 ?>
